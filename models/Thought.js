ebdd61c28404324b5f45e9c633887e6cea5a342a
ElsieMay <elsiemaylawrie1@gmail.com>
2022-06-26 13:16:08 +1000

    moved reaction subdocument to top of thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,37 @@
 const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+// reaction field's subdocument schema in the `Thought` model
+const reactionSchema = new Schema(
+	{
+		reactionId: {
+			type: Schema.Types.ObjectId,
+			default: () => new Types.ObjectId(),
+		},
+		reactionBody: {
+			type: String,
+			required: true,
+			maxlength: 280,
+		},
+		username: {
+			type: String,
+			required: true,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			get: (timestamp) => dateFormat(timestamp),
+		},
+	},
+	{
+		// Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+		// Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+		toJSON: {
+			virtuals: true,
+		},
+		id: false,
+	}
+);
 
 // Schema to create Thought model
 const thoughtSchema = new Schema(
@@ -20,37 +53,6 @@ const thoughtSchema = new Schema(
 		},
 		//Array of nested documents created with the `reactionSchema`
 		reactions: [reactionSchema],
-	},
-	{
-		// Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
-		// Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
-		toJSON: {
-			virtuals: true,
-		},
-		id: false,
-	}
-);
-
-const reactionSchema = new Schema(
-	{
-		reactionId: {
-			type: Schema.Types.ObjectId,
-			default: () => new Types.ObjectId(),
-		},
-		reactionBody: {
-			type: String,
-			required: true,
-			maxlength: 280,
-		},
-		username: {
-			type: String,
-			required: true,
-		},
-		createdAt: {
-			type: Date,
-			default: Date.now,
-			get: (timestamp) => dateFormat(timestamp),
-		},
 	},
 	{
 		// Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
