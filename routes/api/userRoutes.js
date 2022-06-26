cfd4d5fac4409b080896499527597f6abec480e6
ElsieMay <elsiemaylawrie1@gmail.com>
2022-06-26 13:45:32 +1000

    added routes for users

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const { getUsers, getSingleUser, createUser, updateUser, deleteUser, createFriend, deleteFriend } = require("../../controllers/userController");
+
+// /api/users
+router.route("/").get(getUsers).post(createUser);
+
+// /api/users/:usersId
+router.route("/:id").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route("/api/users/:userId/friends/:friendId").post(createFriend).delete(deleteFriend);
+
+module.exports = router;
