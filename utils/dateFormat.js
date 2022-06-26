128ee9149530409a1703a799a4284fc814af731d
ElsieMay <elsiemaylawrie1@gmail.com>
2022-06-26 17:27:15 +1000

    fixed date format

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,10 +1,11 @@
 export function dateFormat(timestamp) {
-	var date = new Date(dateStr);
+	var date = new Date(timestamp);
 
 	var d = date.getDate();
 
 	var m = date.getMonth() + 1;
 
 	var y = date.getFullYear();
+
+	return `${m}/${d}/${y}`;
 }
-return `${m}/${d}/${y}`;
