0ca79c5d088ec5fbbe13e4600adfc1ad488846f5
ElsieMay <elsiemaylawrie1@gmail.com>
2022-06-29 19:49:20 +1000

    removed reaction id in thought model causing error

--- models/Thought.js
+++ models/Thought.js
@@ -4,10 +4,6 @@ const dateFormat = require("../utils/dateFormat");
 // reaction field's subdocument schema in the `Thought` model
 const reactionSchema = new Schema(
 	{
-		reactionId: {
-			type: Schema.Types.ObjectId,
-			default: () => new Types.ObjectId(),
-		},
 		reactionBody: {
 			type: String,
 			required: true,
