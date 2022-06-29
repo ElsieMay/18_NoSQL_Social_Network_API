8a3f5d49bbd0629d5bd51e14003fbbd62ac8aa24
ElsieMay <elsiemaylawrie1@gmail.com>
2022-06-29 16:20:02 +1000

    added date format

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,11 +1,8 @@
-export function dateFormat(timestamp) {
+const dateFormat = (timestamp) => {
 	var date = new Date(timestamp);
-
 	var d = date.getDate();
-
 	var m = date.getMonth() + 1;
-
 	var y = date.getFullYear();
-
 	return `${m}/${d}/${y}`;
-}
+};
+module.exports = dateFormat;
